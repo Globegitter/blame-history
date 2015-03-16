0fbae997d84374f690da0ed8e7128508140333f6
Globegitter <markus.padourek@gmail.com>
2015-03-16 17:21:37 +0000

    Getting the revisions of each file and showing the diff.

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-var Repo = require('git-tools');
-var repo = new Repo('./');
+// var Repo = require('git-tools');
+// var repo = new Repo('./');
+
+var repo = require('simple-git')('./');
 
 module.exports = function () {
-  repo.authors(function (error, authors) {
-    console.log(authors);
+  // repo.authors(function (error, authors) {
+  //   console.log(authors);
+  // });
+  var options = { file: 'index.js' };
+  repo.log(options, (err, log) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    var allLogs = log.all.reverse();
+    var revisions = log.all.length;
+
+    repo.diff(`${allLogs[0].hash} ${allLogs[revisions - 1].hash} -- ${options.file}`, (err, diff) => {
+      if (err) {
+        throw new Error(err);
+      }
+      console.log(diff);
+    });
+
+    // allLogs.reverse().map( (log, i) => {
+    //   if (i < allLogs.length - 1 ) {
+    //     repo.diff(`${log.hash} ${allLogs[i + 1].hash} -- ${options.file}`, (err, diff) => {
+    //       if (err) {
+    //         throw new Error(err);
+    //       }
+    //       console.log(diff);
+    //     });
+    //   }
+    // });
+
   });
 };
