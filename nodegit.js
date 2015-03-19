7bb03f8d941104cff044fa2b7d61dced77ee46a0
Globegitter <markus.padourek@gmail.com>
2015-03-19 20:29:37 +0000

    Output is making more sense now. Showing if line got added or removed.

--- nodegit.js
+++ nodegit.js
@@ -1,4 +1,5 @@
 var Git = require('nodegit');
+var { EOL } = require('os');
 
 module.exports = async function (cmdArgs) {
   // console.log(process.cwd());
@@ -19,17 +20,11 @@ module.exports = async function (cmdArgs) {
   history.on('commit', async function (commit) {
     var diffList = await commit.getDiff();
     var filePath = '';
-    console.log('whaaaa');
     for (var diff of diffList) {
       for (var patch of diff.patches()) {
         //not sure why this check oldFile and newFile path, but the example did that.
         var oldFilePath = patch.oldFile().path();
         var newFilePath = patch.newFile().path();
-        console.log('---test---');
-        // console.log();
-        console.log();
-        console.log();
-        // console.log(patch.newFile().toString());
         if (newFilePath.includes(fileName)) {
           filePath = newFilePath;
         } else if (oldFilePath.includes(fileName)) {
@@ -38,13 +33,26 @@ module.exports = async function (cmdArgs) {
 
         //found the file we are looking for
         if (filePath.length > 0) {
+          console.log();
+          console.log();
+          console.log(`Found file in commit ${commit.sha()}`);
+          console.log("Author:", commit.author().name() +
+         " <" + commit.author().email() + ">");
+          console.log("Date:", commit.date());
+          console.log("\n    " + commit.message());
+          console.log(`Showing file ${filePath}`);
           //geting the file content
           for (var hunk of patch.hunks()) {
-            console.log(hunk.header().trim());
+            console.log('header', hunk.header().trim());
+            console.log('size', hunk.size());
+            var count = 1;
             //getting the file line-by-line
             for (var line of hunk.lines()) {
-              console.log('lineOrigin', String.fromCharCode(line.origin()));
-              console.log('content', line.content().trim());
+              var content = line.content();
+              // console.log('lineOrigin', String.fromCharCode(line.origin()));
+              console.log(count, String.fromCharCode(line.origin()) + ' ' + content.slice(0, content.indexOf(EOL)));
+              // console.log(count, String.fromCharCode(line.origin()) + ' ' + content);
+              count++;
             }
           }
           //found file so break out of for loop
